9405f88c3f208c98fdfb6bf6e9d807c05eed24f3
Artem P. <parkhomov@gmail.com>
2025-10-21 22:15:18 +0200

    Vite - updated configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,23 @@ export default defineConfig({
         port: 8080,
         open: '/index.html',
     },
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+        cssCodeSplit: false,
+        rollupOptions: {
+            input: 'index.html',
+            output: {
+                entryFileNames: 'bundle.js',
+                chunkFileNames: 'bundle.js',
+                assetFileNames: (assetInfo) => {
+                    const firstName = assetInfo.names?.[0] ?? '';
+                    if (firstName.endsWith('.css')) {
+                        return 'bundle.css';
+                    }
+                    return '[name][extname]';
+                },
+            },
+        },
+    },
 });
